8294a25b015703fa9b5fd7bd49d7637c664fb920
SteoType <y7ipr8aax9sahy@gmail.com>
2026-05-11 22:52:05 +0700

    Update reseller.js

--- functions/api/reseller.js
+++ functions/api/reseller.js
@@ -1,23 +1,21 @@
 export async function onRequest(context) {
-  // 1. Tangkap semua parameter dari website (?action=login atau ?action=get_stok)
+  // Tangkap semua parameter (termasuk token keamanan yang baru)
   const url = new URL(context.request.url);
   const queryString = url.search; 
 
-  // 2. Tempelkan ke URL Asli Google Script kamu (yang disembunyikan)
+  // LINK GOOGLE SCRIPT BARU (Sistem Keamanan & Token)
   const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxUQQKI2MNo3jqESMvdV1O7prnqVm34SBKtXduzkExaCwR6k1m2tPggh5_CmHxwSWXg/exec" + queryString;
 
   try {
-    // 3. Ambil data dari Google secara diam-diam
     const response = await fetch(GOOGLE_SCRIPT_URL);
     const data = await response.json();
 
-    // 4. Kirim hasilnya ke website
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Cache-Control": "no-store" // Memastikan data login/stok selalu baru
+        "Cache-Control": "no-store" // Memastikan harga selalu update
       }
     });
   } catch (error) {
